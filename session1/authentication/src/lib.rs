ba1a237dbb2bba927d1f62a90c31f993b08e257d
mobamoh <b4m0h4m3d@gmail.com>
2024-04-11 15:20:13 +0000

    enums, structs, vectors, hashmap, serde, sha2

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -1,9 +1,113 @@
+use std::{collections::HashMap, path::Path};
+
+use serde::{Deserialize, Serialize};
+
 pub fn greet(name: &str) -> String {
     format!("Welcome back {name}!")
 }
 
-pub fn login(username: &str, password: &str) -> bool {
-    username.to_lowercase() == "admin" && password == "nimda"
+fn hash_password(password: &str) -> String {
+    use sha2::Digest;
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(password);
+    format!("{:X}", hasher.finalize())
+}
+
+pub enum LoginAction {
+    Granted(LoginRole),
+    Denied,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub enum LoginRole {
+    Admin,
+    User,
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    username: String,
+    password: String,
+    role: LoginRole,
+}
+
+impl User {
+    fn new(username: &str, password: &str, role: LoginRole) -> Self {
+        Self {
+            username: username.to_lowercase(),
+            password: hash_password(password),
+            role,
+        }
+    }
+}
+
+// fn get_users() -> Vec<User> {
+//     vec![
+//         User::new("admin", "nimda", LoginRole::Admin),
+//         User::new("moha", "bamoh", LoginRole::User),
+//     ]
+// }
+
+fn get_example_users() -> HashMap<String, User> {
+    let mut users = HashMap::new();
+    users.insert(
+        "admin".to_string(),
+        User::new("admin", "nimda", LoginRole::Admin),
+    );
+    users.insert(
+        "moha".to_owned(),
+        User::new("moha", "bamoh", LoginRole::User),
+    );
+    users
+}
+
+fn get_users() -> HashMap<String, User> {
+    let users_path = Path::new("users.json");
+    if users_path.exists() {
+        let users_json = std::fs::read_to_string(users_path).unwrap();
+        let users: HashMap<String, User> = serde_json::from_str(&users_json).unwrap();
+        users
+    } else {
+        let users = get_example_users();
+        let users_json = serde_json::to_string(&users).unwrap();
+        std::fs::write(users_path, users_json).unwrap();
+        users
+    }
+}
+
+// fn get_admin_usernames() -> Vec<String> {
+//     get_users()
+//         .into_iter() // into_iter moves ownership
+//         .filter(|u| u.role == LoginRole::Admin)
+//         .map(|u| u.username)
+//         .collect()
+// }
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let username_lower = username.to_lowercase();
+    let password = hash_password(password);
+    // lookup by hashkey is faster than iterating over vector
+    if let Some(user) = get_users().get(&username_lower) {
+        // if let Some(user) = get_users().iter().find(|u| u.username == username_lower) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()));
+        } else {
+            return Some(LoginAction::Denied);
+        }
+    }
+    None
+
+    // if username.to_lowercase() != "admin" && username.to_lowercase() != "moha" {
+    //     return None;
+    // }
+
+    // if username.to_lowercase() == "admin" && password == "nimda" {
+    //     Some(LoginAction::Granted(LoginRole::Admin))
+    // } else if username.to_lowercase() == "moha" && password == "ahom" {
+    //     Some(LoginAction::Granted(LoginRole::User))
+    // } else {
+    //     Some(LoginAction::Denied)
+    // }
 }
 
 #[cfg(test)]
@@ -11,15 +115,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_greet(){
-        assert_eq!("Welcome back Mohamed!",greet("Mohamed"))
+    fn test_greet() {
+        assert_eq!("Welcome back Mohamed!", greet("Mohamed"))
     }
 
     #[test]
-    fn test_login(){
-        assert!(login("admin", "nimda"));
-        assert!(login("AdMIn", "nimda"));
-        assert!(!login("username", "nimda"));
-        assert!(!login("admin", "password"))
+    fn test_login() {
+        // assert!(login("admin", "nimda"));
+        // assert!(login("AdMIn", "nimda"));
+        // assert!(!login("username", "nimda"));
+        // assert!(!login("admin", "password"))
     }
 }
--- session1/login/src/main.rs
+++ session1/login/src/main.rs
@@ -1,33 +1,61 @@
 use std::io;
 
-use authentication::{greet, login};
+use authentication::{greet, login, LoginAction, LoginRole};
 
 fn main() {
     let mut tries = 0;
-    println!("🦀🦀🦀🦀🦀🦀🦀 🔐🔐 🦀🦀🦀🦀🦀🦀🦀"); 
+    println!("🦀🦀🦀🦀🦀🦀🦀 🔐🔐 🦀🦀🦀🦀🦀🦀🦀");
     loop {
         println!("Enter your username: ");
         let username = read_input();
         println!("Enter your password: ");
         let password = read_input();
 
-        if login(&username, &password) {
-            println!("{}", greet(&username));
-            break;
-        } else {
-            println!("Username or Password incorrect! 🤷‍♂️");
-            tries += 1;
-            if tries >3 {
-                println!("Your account is locked, please contact the admin!");
+        match login(&username, &password) {
+            Some(LoginAction::Granted(role)) => {
+                println!("{}", greet(&username));
+                match role {
+                    LoginRole::Admin => {
+                        println!("You are logged in as an ADMIN!")
+                    }
+                    LoginRole::User => {
+                        println!("You are logged in as a User!")
+                    }
+                };
                 break;
             }
-        }
+            Some(LoginAction::Denied) => {
+                println!("Username or Password incorrect! 🤷‍♂️");
+                tries += 1;
+                if tries > 3 {
+                    println!("Your account is locked, please contact the admin!");
+                    break;
+                }
+            }
+            None => {
+                println!("You need to create an account!");
+                break;
+            }
+        };
+
+        // if login(&username, &password) {
+        //     println!("{}", greet(&username));
+        //     break;
+        // } else {
+        //     println!("Username or Password incorrect! 🤷‍♂️");
+        //     tries += 1;
+        //     if tries > 3 {
+        //         println!("Your account is locked, please contact the admin!");
+        //         break;
+        //     }
+        // }
     }
 }
 
-
-fn read_input() -> String{
+fn read_input() -> String {
     let mut buf = String::new();
-    io::stdin().read_line(&mut buf).expect("Error reading stdin!");
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Error reading stdin!");
     buf.trim().to_string()
 }
